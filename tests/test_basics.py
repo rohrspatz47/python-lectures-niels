6c64d68d28fee38a75748f9243f7ae55c3d6b178
Niels Hoppe <mail@nielshoppe.de>
2024-05-15 14:28:45 +0200

    Fix typo

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -8,8 +8,8 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(3, 5), 8)
 
-    def test_substract(self):
-        self.assertEqual(substract(8, 5), 3)
+    def test_subtract(self):
+        self.assertEqual(subtract(8, 5), 3)
 
     def test_multiply(self):
         self.assertEqual(multiply(4, 6), 24)
